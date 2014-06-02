4f6cf3cddd20494d53f65a62fa86488f55b25719
jkoomjian <jkoomjian@gmail.com>
2014-06-02 16:13:37 -0700

    fixing path

--- src/loader_bookmarklet.js
+++ src/loader_bookmarklet.js
@@ -14,8 +14,8 @@ function _pgzpInitBookmarklet() {
 	pgzp().win = window;
 	pgzp().doc = window.document;
 	pgzp().loader_type = "bookmarklet";
-	pgzp().media_path = "http://127.0.0.1/pgzp/src/ffextension/skin/";
-	//pgzp().media_path = "http://www.printwhatyoulike.com/static/pagezipper/ui/";
+	//pgzp().media_path = "http://127.0.0.1/pgzp/src/ffextension/skin/";
+	pgzp().media_path = "http://www.printwhatyoulike.com/static/pagezipper/ui/";
 	pgzp().loadPageZipper();
 }
 

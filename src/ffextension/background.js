a26e0a95fae9144fc95afb7f990fd9910382c1e4
Jonathan <jkoomjian@gmail.com>
2016-07-07 13:28:39 -0700

    fix issue w/extension not starting after page reload

--- src/ffextension/background.js
+++ src/ffextension/background.js
@@ -1,7 +1,8 @@
-var loaded_tabs = {}, icon_src = "";
+var loaded_tabs = {};
 
 //Pgzp icon is clicked
 browser.browserAction.onClicked.addListener(function(tab) {
+	var icon_src = "";
 
 	if (!loaded_tabs[tab.id]) {
 		browser.tabs.executeScript(tab.id, {'file': 'jquery.js'}, function () {
@@ -26,16 +27,9 @@ browser.browserAction.onClicked.addListener(function(tab) {
 
 // New page is loaded in tab
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-
-	//deactivate pgzp button on page load
-	delete loaded_tabs[tabId];
-
-	//TODO on tab change
-	// if (localStorage["extensions.pagezipper.autorun"] == "true" && changeInfo.status == "complete" && !loaded_tabs[tabId]) {
-	// 	//load pgzp
-	// 	loaded_tabs[tabId] = "off";
-	// 	chrome.tabs.executeScript(tab.id, {'file': 'pagezipper.js'}, function () {
-	// 		chrome.tabs.executeScript(tab.id, {'code': '_pgzpAutorun()'});
-	// 	});
-	// }
+	if (changeInfo.status == "complete" && loaded_tabs[tabId]) {
+		// deactivate pgzp button on page load - restarting pgzp in this tab will require reloading everything
+		delete loaded_tabs[tabId];
+		browser.browserAction.setIcon({tabId: tab.id, path: browser.extension.getURL("icon19.png")});
+	}
 });

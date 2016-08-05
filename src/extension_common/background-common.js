1ae4969f313f417fc43eaf7395b59e80f4daac26
Jonathan <jkoomjian@gmail.com>
2016-08-05 16:16:08 -0700

    bug fix

--- src/extension_common/background-common.js
+++ src/extension_common/background-common.js
@@ -7,13 +7,16 @@ function updateActiveTab(tabId, changeInfo, tab) {
 	if (changeInfo.status == "complete" && loaded_tabs[tabId]) {
 		// deactivate pgzp button on page load - restarting pgzp in this tab will require reloading everything
 		delete loaded_tabs[tabId];
-		chrome.browserAction.setIcon({tabId: tab.id, path: chrome.extension.getURL("icon19.png")});
+		chrome.browserAction.setIcon({tabId: tab.id, path: chrome.extension.getURL("extension_icons/icon19.png")});
 	}
 }
 
 function autoRun(details) {
 	//ensure pgzp is not already running in this tab
 	if (loaded_tabs[details.tabId]) return;
+
+	//ignore frames - tabId is important to keep - otherwise frames will break everything
+  	if (details.frameId !== 0) return;
 
 	var url = details.url;
 	var currTab = {id: details.tabId};
@@ -28,7 +31,7 @@ function autoRun(details) {
 /*-------------------- Event Handlers -----------------*/
 
 //Update media queries after page load
-chrome.webNavigation.onCompleted.addListener(autoRun);
+chrome.webNavigation.onDOMContentLoaded.addListener(autoRun); //
 
 // Run Pgzp when the toolbar button is clicked
 chrome.browserAction.onClicked.addListener(runPgzp);
